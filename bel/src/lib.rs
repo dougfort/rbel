b34ef0d5cd902eec2ad961b0d1adcb66c90da2f2
dougfort <doug.fort@deciphernow.com>
2019-10-20 08:55:33 -0400

    Start parse (#1)
    * experiment with parsing

--- bel/src/lib.rs
+++ bel/src/lib.rs
@@ -1,12 +1,17 @@
 pub mod object;
+pub mod parser;
+use failure::{format_err, Error};
+use object::*;
 
-pub struct Bel {
-
-}
+pub struct Bel {}
 
 impl Bel {
     pub fn new() -> Self {
-        Bel{}
+        Bel {}
+    }
+
+    pub fn eval(&mut self, _obj: &Object) -> Result<Object, Error> {
+        Err(format_err!("eval not implemented"))
     }
 }
 
--- repl/src/main.rs
+++ repl/src/main.rs
@@ -1,7 +1,43 @@
 use bel::object::Object;
+use bel::parser::Parser;
 use bel::Bel;
+use failure::Error;
+use std::io::{stdin, stdout, Write};
 
-fn main() {
-    let _bel = Bel::new();
-    println!("Hello, world! {}", Object::Nil);
+fn main() -> Result<(), Error> {
+    let parser = Parser::new();
+    let mut bel = Bel::new();
+    loop {
+        let stdin_line = get_stdin_line(">")?;
+        let line = stdin_line.trim();
+        match line {
+            ":q" => {
+                println!("break!");
+                break;
+            }
+
+            _ => match parser.parse(line) {
+                Ok(parse_result) => {
+                    println!("parse result = {}", parse_result);
+                    let eval_result = bel.eval(&parse_result)?;
+                    println!("eval result = {}", eval_result);
+                }
+                Err(e) => {
+                    eprintln!("parse error: {}", e);
+                }
+            },
+        }
+    }
+    Ok(())
+}
+
+fn get_stdin_line(prompt: &str) -> std::io::Result<String> {
+    println!("");
+    print!("{} ", prompt);
+    stdout().flush()?;
+
+    let mut input = String::new();
+    stdin().read_line(&mut input)?;
+
+    Ok(input)
 }
