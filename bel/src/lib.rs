6151a60dea2526c00ad7334872bb9b877b8e8578
dougfort <doug.fort@gmail.com>
2022-01-21 15:30:44 -0500

    progress on list

--- bel/src/lib.rs
+++ bel/src/lib.rs
@@ -1,35 +1,40 @@
 pub mod parser;
-use thiserror::Error;
 use std::fmt;
+use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum BelError {
     #[error("bel error")]
-    Error, 
+    Error,
 }
 
 /// Bel has four fundamental data types:
 /// symbols, pairs, characters, and streams.
 /// Instances of the four fundamental types are called objects
 /// https://sep.yimg.com/ty/cdn/paulgraham/bellanguage.txt
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Object {
+    Nil,
     Symbol(String),
-    Pair(Box<(Object, Object)>),
-    Char(Vec<u8>),
+    List(Vec<Object>),
+    Char(String),
     Stream,
+}
+
+impl Object {
+    pub fn is_nil(&self) -> bool {
+        self == &Object::Nil
+    }
 }
 
 impl fmt::Display for Object {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         use self::Object::*;
         match &self {
+            Nil => write!(f, "nil"),
             Symbol(word) => write!(f, "{}", word),
-            Pair(pair) => {
-                let unboxed_pair = &*pair;
-                write!(f, "({}, {})", unboxed_pair.0, unboxed_pair.1)
-            }
-            Char(c) => write!(f, "\\{:?}", c),
+            List(l) => write!(f, "{:?}", l),
+            Char(c) => write!(f, "\\{}", c),
             Stream => write!(f, "<stream>"),
         }
     }
--- bel/src/parser.rs
+++ bel/src/parser.rs
@@ -1,6 +1,4 @@
 use crate::Object;
-use nom::{IResult, Finish};
-use nom::sequence::preceded;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -9,38 +7,197 @@ pub enum ParseError {
     Error(String),
 }
 
-pub fn parse(input: &str) -> Result<Object, ParseError> {
-    let input = input.trim();
-    match char(input) {
-        Ok(res) => println!("{:?}", res),
-        Err(e) =>  println!("{:?}", e),
-    }
-    Err(ParseError::Error("not implements".to_string()))
+enum State {
+    ConsumeWhitespace,
+    BuildSymbol,
+    BuildChar,
 }
 
-fn char(input: &str) -> IResult<&str, &str> {
-    Ok(("", ""))
+pub fn parse(input: &str) -> Result<Vec<Object>, ParseError> {
+    let mut level = 0;
+    let mut accum = String::new();
+    // start with an outer list whether we need it or not
+    let mut list_stack: Vec<Vec<Object>> = vec![Vec::new()];
+    let mut state = State::ConsumeWhitespace;
+    let mut current_list: Vec<Object> = Vec::new();
+    for c in input.chars() {
+        match c {
+            '(' => {
+                match state {
+                    State::BuildSymbol => {
+                        current_list.push(Object::Symbol(accum.clone()));
+                    }
+                    State::BuildChar => {
+                        current_list.push(Object::Char(accum.clone()));
+                    }
+                    _ => {}
+                }
+                list_stack.push(current_list);
+                current_list = Vec::new();
+                state = State::ConsumeWhitespace;
+                level += 1;
+            }
+            ')' => {
+                match state {
+                    State::BuildSymbol => {
+                        current_list.push(Object::Symbol(accum.clone()));
+                    }
+                    State::BuildChar => {
+                        current_list.push(Object::Char(accum.clone()));
+                    }
+                    _ => {}
+                }
+                level -= 1;
+                list_stack[level].push(Object::List(current_list));
+                list_stack.pop();
+                current_list = Vec::new();
+                state = State::ConsumeWhitespace;
+            }
+            '\\' => {
+                accum.clear();
+                state = State::BuildChar;
+            }
+            _ => match state {
+                State::BuildSymbol if c.is_whitespace() => {
+                    current_list.push(Object::Symbol(accum.clone()));
+                    state = State::ConsumeWhitespace;
+                }
+                State::BuildChar if c.is_whitespace() => {
+                    current_list.push(Object::Char(accum.clone()));
+                    state = State::ConsumeWhitespace;
+                }
+                State::ConsumeWhitespace if c.is_whitespace() => {}
+                State::ConsumeWhitespace => {
+                    accum.clear();
+                    accum.push(c);
+                    state = State::BuildSymbol;
+                }
+                _ => {
+                    accum.push(c);
+                }
+            },
+        }
+        println!(
+            "c = '{}', accum = '{}', current_list = {:?}, (level, len) = ({}, {}), list_stack = {:?}", 
+            c, accum, current_list, level, list_stack.len(), list_stack
+        );
+    }
+    if level > 0 {
+        Err(ParseError::Error(format!("invalid level: {}", level)))
+    } else {
+        println!(
+            "final, accum = '{}', current_list = {:?}, (level, len) = ({}, {}), list_stack = {:?}",
+            accum,
+            current_list,
+            level,
+            list_stack.len(),
+            list_stack
+        );
+        match state {
+            State::BuildSymbol => {
+                list_stack[0].push(Object::Symbol(accum.clone()));
+            }
+            State::BuildChar => {
+                list_stack[0].push(Object::Char(accum.clone()));
+            }
+            _ => {}
+        }
+        Ok(list_stack[0].clone())
+    }
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn parse_fails() {
-        assert!(parse("x").is_err());
+    fn can_parse_empty_string() {
+        assert_eq!(parse("").unwrap(), vec![]);
     }
 
     #[test]
-    fn can_parse_char() {
-        match parse("\\a") {
-            Ok(obj) => {
-                if let Object::Char(c) = obj {
-                    assert_eq!(c, &[b'a']);
+    fn can_parse_symbol() {
+        assert_eq!(parse("a").unwrap(), vec![Object::Symbol("a".to_string())]);
+    }
+
+    #[test]
+    fn can_parse_empty_list() {
+        let mut res = parse("()").unwrap();
+        match res.pop() {
+            Some(object) => {
+                if let Object::List(l) = object {
+                    assert!(l.is_empty());
                 } else {
-                    panic!("unknown object: {:?}", obj)
-                }                
-            },
-            Err(e) => panic!("{:?}", e),
+                    panic!("unexpected return type: {:?}", res);
+                }
+            }
+            None => {
+                panic!("unexpected empty list: {:?}", res);
+            }
+        }
+    }
+
+    #[test]
+    fn can_parse_list_of_symbols() {
+        let mut res = parse("(a b)").unwrap();
+        match res.pop() {
+            Some(object) => {
+                if let Object::List(l) = object {
+                    assert_eq!(
+                        l,
+                        vec![
+                            Object::Symbol("a".to_string()),
+                            Object::Symbol("b".to_string())
+                        ]
+                    );
+                } else {
+                    panic!("unexpected return type: {:?}", res);
+                }
+            }
+            None => {
+                panic!("unexpected empty list: {:?}", res);
+            }
+        }
+    }
+
+    #[test]
+    fn can_parse_embedded_list() {
+        let mut res = parse("((x))").unwrap();
+        match res.pop() {
+            Some(object) => {
+                if let Object::List(l) = object {
+                    assert_eq!(l, vec![Object::List(vec![Object::Symbol("c".to_string())])]);
+                } else {
+                    panic!("unexpected return type: {:?}", res);
+                }
+            }
+            None => {
+                panic!("unexpected empty list: {:?}", res);
+            }
+        }
+    }
+
+    #[test]
+    fn can_parse_embedded_list_of_symbols() {
+        let mut res = parse("(a b (c))").unwrap();
+        match res.pop() {
+            Some(object) => {
+                if let Object::List(l) = object {
+                    assert_eq!(
+                        l,
+                        vec![
+                            Object::Symbol("a".to_string()),
+                            Object::Symbol("b".to_string()),
+                            Object::List(vec![Object::Symbol("c".to_string())]),
+                        ]
+                    );
+                } else {
+                    panic!("unexpected return type: {:?}", res);
+                }
+            }
+            None => {
+                panic!("unexpected empty list: {:?}", res);
+            }
         }
     }
 }
